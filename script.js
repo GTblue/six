7ecd612dea8e78c6e0ae156abde514e39a01d195
Gregory Monroe II <gregthemusician@gmail.com>
2020-06-17 23:53:55 -0400

    connected coordinated information between HTML & JS

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     // Define APIs
     // Start building URLS
     // When City name is entered, url comes back. 
-    console.log("Hello World");
 
     var APIkey = "9caef82cbea05a5c8889026ba089056e";
     var queryURL = 
@@ -15,18 +14,50 @@ $(document).ready(function(){
           .then(function (response) {
             console.log(response);
         
+        $("#city").text(response.city.name)  
+        console.log(response.city.name)
+
         $("#temperature").text(response.list["0"].main.temp)  
-        console.log(response.list["0"].main.humidity)
+        console.log(response.list["0"].main.temp)
 
         $("#humidity").text(response.list["0"].main.humidity)  
         console.log(response.list["0"].main.humidity)
 
         $("#windspeed").text(response.list["0"].wind.speed)  
-        console.log(response.list["0"].main.humidity)
+        console.log(response.list["0"].wind.speed)
 
-        $("#uvindex").text(response.list["0"].main.humidity)  
-        console.log(response.list["0"].main.humidity)
+        $("#uvindex").text(response.list["0"].main)  
+        console.log(response.list["0"].main)
 
+        $("#temp1").text(response.list["1"].main.temp)  
+        console.log(response.list["1"].main.temp)
+
+        $("#humid1").text(response.list["1"].main.humidity)  
+        console.log(response.list["1"].main.humidity)
+        
+        $("#temp2").text(response.list["2"].main.temp)  
+        console.log(response.list["1"].main.temp)
+
+        $("#humid2").text(response.list["2"].main.humidity)  
+        console.log(response.list["2"].main.humidity)
+
+        $("#temp3").text(response.list["3"].main.temp)  
+        console.log(response.list["3"].main.temp)
+
+        $("#humid3").text(response.list["3"].main.humidity)  
+        console.log(response.list["3"].main.humidity)
+
+        $("#temp4").text(response.list["4"].main.temp)  
+        console.log(response.list["4"].main.temp)
+
+        $("#humid4").text(response.list["4"].main.humidity)  
+        console.log(response.list["4"].main.humidity)
+
+        $("#temp5").text(response.list["5"].main.temp)  
+        console.log(response.list["5"].main.temp)
+
+        $("#humid5").text(response.list["5"].main.humidity)  
+        console.log(response.list["5"].main.humidity)
             //create a function that has an element called
     })
       //create a function that has an element calle
